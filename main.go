451a4e4e98b27d4d0e19aaec494a6d781569dedf
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-09-09 12:34:09 +0530

    prepare data to start mining

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/big"
 	"strconv"
 	"time"
 )
@@ -18,8 +21,54 @@ type BlockChain struct {
 	blocks []*Block
 }
 
-func main() {
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
 
+const targetBits = 24
+
+func main() {
+	bc := NewBlockChain()
+
+	bc.AddBlock("Send 1 ETH to alom")
+	bc.AddBlock("Send 1.5 ETH to 6wong")
+	bc.AddBlock("Send 2 ETH to achi hoon")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Previous hash: %x\n", block.PreviousBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, 256-targetBits)
+
+	pow := &ProofOfWork{
+		block:  b,
+		target: target,
+	}
+	return pow
+}
+
+func IntToHex(data int64) []byte {
+	hexString := fmt.Sprintf("%x", data)
+	return []byte(hexString)
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join([][]byte{
+		pow.block.PreviousBlockHash,
+		pow.block.Data,
+		IntToHex(pow.block.Timestamp),
+		IntToHex(int64(targetBits)),
+		IntToHex(int64(nonce)),
+	}, []byte{})
+
+	return data
 }
 
 func (b *Block) setHash() {
