72754945a7d9222f0e9345dfe17eb5313835ecd2
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-09-13 15:17:25 +0530

    implement functions to serialize and deserialize the block data for database storage

---
+++ block.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// transforms incoming data to binary data to be transmitted further
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	encoder.Encode(b)
+
+	return result.Bytes()
+}
+
+// does the opposite of Serialize
+func Deserialize(data []byte) Block {
+	var block Block
+
+	reader := bytes.NewReader(data)
+	decoder := gob.NewDecoder(reader)
+	decoder.Decode(&block)
+
+	return block
+}
