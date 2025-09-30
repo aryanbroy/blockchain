93ef68d8c079e267edc93987371b4a5d12f9bb6c
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-09-30 13:08:23 +0530

    fetch balance for particular address

--- cli.go
+++ cli.go
@@ -23,6 +23,49 @@ func (cli *CLI) printUsage() {
 // 	cli.bc.MineBlock(data)
 // 	fmt.Println("Success")
 // }
+//
+
+func (cli *CLI) getBalance(address string) {
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panicln("Error opening db file")
+	}
+
+	var genesisBlockHash []byte
+
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			log.Println("No bucket found with name: ", bucket)
+			return err
+		}
+		genesisBlockHash = b.Get([]byte("l"))
+		return nil
+	})
+	if err != nil {
+		log.Panicln("Error starting a read only transaction")
+	}
+
+	bc := &Blockchain{
+		tip: genesisBlockHash,
+		db:  db,
+	}
+	cli.bc = bc
+
+	unspentoutputs := cli.bc.FindUTXOs(address)
+	balance := 0
+
+	for _, output := range unspentoutputs {
+		balance += output.Value
+	}
+
+	fmt.Printf("Balance of %s: %v\n", address, balance)
+
+	err = bc.db.Close()
+	if err != nil {
+		log.Panicln("Error closing the db while fetching the balance")
+	}
+}
 
 func (cli *CLI) printChain() {
 	db, err := bolt.Open(dbFile, 0600, nil)
@@ -83,8 +126,10 @@ func (cli *CLI) createBlockchain(address string) {
 func (cli *CLI) Run() {
 	printChainCmd := flag.NewFlagSet("printChain", flag.ExitOnError)
 	createBlockChainCmd := flag.NewFlagSet("createBlockChain", flag.ExitOnError)
+	getBalCmd := flag.NewFlagSet("getBalance", flag.ExitOnError)
 
 	createBlockChainAddress := createBlockChainCmd.String("address", "", "The address to send the reward for mining the genesis block")
+	getBalAddress := getBalCmd.String("address", "", "Address to fetch balance for")
 
 	if len(os.Args) < 2 {
 		cli.printUsage()
@@ -100,9 +145,17 @@ func (cli *CLI) Run() {
 		if err := createBlockChainCmd.Parse(os.Args[2:]); err != nil {
 			log.Panicln("Error parsing the createblockchain command")
 		}
+	case "getBalance":
+		if err := getBalCmd.Parse(os.Args[2:]); err != nil {
+			log.Panicln("Error parsing the getBalance command")
+		}
 	default:
 		cli.printUsage()
 		os.Exit(1)
+	}
+
+	if getBalCmd.Parsed() {
+		cli.getBalance(*getBalAddress)
 	}
 
 	if printChainCmd.Parsed() {
